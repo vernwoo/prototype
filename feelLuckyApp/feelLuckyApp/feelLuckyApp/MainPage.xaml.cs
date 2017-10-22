b9141470a6826d0956f363718c62d1a1b9848be7
vernwoo <vernwoo@gmail.com>
2017-10-22 18:04:01 +0800

    Feel lucky app: Android and IOS mobile app which allows you to see your luck by returning a random fortune cookie phrase. 1st checkin: basic app tested on Android simulator. Lucky phrase is hardcoded for now.

---
+++ feelLuckyApp/feelLuckyApp/feelLuckyApp/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace feelLuckyApp
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+        private void OnButtonClicked(object sender, EventArgs args)
+        {
+            labelMsg.Text = "You win a jackpot!";
+        }
+    }
+}
